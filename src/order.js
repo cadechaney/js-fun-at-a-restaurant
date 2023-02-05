d70ebd246151b8512ad95fe392edb665b6853c18
Cade Chaney <cadechaney@live.com>
2023-02-04 20:18:32 -0600

    Add answers to refund order

--- src/order.js
+++ src/order.js
@@ -1,7 +1,35 @@
 
+function takeOrder (orderNum, deliveryOrders) {
+ 
+  if (deliveryOrders.length < 3) {
+     deliveryOrders.push(orderNum)
+  } else { 
+    return deliveryOrders
+  }
+}  
+
+//function refundOrder (orderNum, deliveryOrders) {
+  //console.log(deliveryOrders[1].orderNumber )
+  //console.log(deliveryOrders)
+  //console.log(orderNum)
+  //console.log(deliveryOrders.splice(orderNum))
+  function refundOrder(orderNumber, deliveryOrders) {
+    for (var i = 0; i < deliveryOrders.length; i++) {
+      if (deliveryOrders[i].orderNumber === orderNumber) {
+        deliveryOrders.splice(i, 1);
+      }
+  }
+  return deliveryOrders;
+}
+    
+
+
+
+
+
 module.exports = {
-  // takeOrder,
-  // refundOrder,
+   takeOrder,
+  refundOrder,
   // listItems,
   // searchOrder
 }
