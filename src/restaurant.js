627cf12076cbde47e2eb49e877a8b2ad858a6991
Cade Chaney <cadechaney@live.com>
2023-02-05 20:13:05 -0600

    Add function for addMenuItem

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,42 @@
 
+function createRestaurant(restaurantName) {
+  return {
+    name: restaurantName,
+    menus: {
+      breakfast: [],
+      lunch: [],
+      dinner: []
+    }
+  }
+}
+
+// function addMenuItem(restaurant, foodItem) {
+//   for (var i = 0; i < restaurant.menus[foodItem.type].length; i++) {
+//    if (restaurant.menus[foodItem.type][i] === foodItem) {
+//       return restaurant.menus
+//     }
+// }  
+// restaurant.menus[foodItem.type].push(foodItem)
+// return restaurant.menus
+// }
+
+function addMenuItem(restaurant, foodItem) {
+  for (var i = 0; i < restaurant.menus[foodItem.type].length; i++) {
+    if (restaurant.menus[foodItem.type][i] === foodItem) {
+      return restaurant.menus;
+    }
+  }
+  restaurant.menus[foodItem.type].push(foodItem);
+  return restaurant.menus;
+}
+
+function removeMenuItem (restaurant, foodItem, foodType) {
+  for ()
+}
+
 
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
-  // removeMenuItem
+  createRestaurant, 
+  addMenuItem,
+  removeMenuItem
 }
